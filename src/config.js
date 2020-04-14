c943f96935fd58947fb4da2f1a518bc848f27eac
Ron Martin <ronmartin89@gmail.com>
2020-04-14 07:40:36 -0700

    fixed a bug where it wasnt redirecting to the dashboard after login

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://evening-bayou-01801.herokuapp.com/api',
+  API_ENDPOINT: 'http://localhost:8000/api' || 'https://evening-bayou-01801.herokuapp.com/api',
   TOKEN_KEY: process.env.TOKEN_KEY,
 }
