9b915e02b84d14eacc61461987c17d52161713ab
Ron Martin <ronmartin89@gmail.com>
2020-04-14 11:06:26 -0700

    made the lang-service, finished the Dashboard components structure, and most of the CSS.

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import Button from '../Button/Button';
 import './Dashboard.css';
+import DashboardWord from '../DashboardWord/DashboardWord';
 
 export default class Dashboard extends Component{
 
     state = { error: null}
+
+    componentDidMount() {
+
+    }
 
     render() {
         const { error } = this.state
@@ -16,14 +21,18 @@ export default class Dashboard extends Component{
 
                 <div className="main-card">
                     <h1>Learn LANGUAGE-NAME-HERE!</h1>
-                    <p>Learning through spaced repetition will help you remember for good! To get started log in or sign up がんばれ！</p>
-                
-                    <Button type='submit' className='log-in-button'>
-                      Start Practicing
-                    </Button>
+                    <p>ひらがなを習う</p>
 
-                    <div className="">
-                    
+                    <div className="practice-button-container">
+                        <Button onClick={null} className='practice-button'>
+                        Start Practicing
+                        </Button>
+                    </div>
+                    <p>Whats next to pratice:</p>
+                    <div className="word-card">
+                        <DashboardWord />
+                        <DashboardWord />
+                        <DashboardWord />
                     </div>
                 </div>
 
---
+++ src/components/DashboardWord/DashboardWord.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './DashboardWord.css';
+
+export default function DashboardWord(props) {
+    return(
+        <div>
+            <p>Japanese word here</p>
+           <p>{`Times right: NUM_RIGHT Times wrong: NUM_WRONG`}</p>
+        </div>
+    );
+} 
