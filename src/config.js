588c25072fb3a0d49646375007fb94e6a939a517
Lillian Burnside <lillian.burnside@gmail.com>
2020-04-17 14:26:46 -0500

    updated heroku link

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT:'https://evening-bayou-01801.herokuapp.com/api',
+  API_ENDPOINT:'https://glacial-brook-22925.herokuapp.com/api',
   // TOKEN_KEY: process.env.TOKEN_KEY,
   TOKEN_KEY:"super_secret_key"
 }
